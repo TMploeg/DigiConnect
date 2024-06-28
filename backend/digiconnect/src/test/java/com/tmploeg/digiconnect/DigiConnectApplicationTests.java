8f38254d88ed602a5a5e6b72f1d4e38fb63e1461
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-06-28 10:14:34 +0200

    created backend project

---
+++ backend/digiconnect/src/test/java/com/tmploeg/digiconnect/DigiConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmploeg.digiconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
